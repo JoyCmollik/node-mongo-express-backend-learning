5b9897ec32ce30701ec893de6796b75c39e470ae
Joy Chandra Mollik <xoycmollik913@gmail.com>
2022-08-30 23:10:11 +0600

    added jobs api

--- 06-jobs-api/starter/middleware/authentication.js
+++ 06-jobs-api/starter/middleware/authentication.js
@@ -14,6 +14,10 @@ const authenticationMiddleware = async (req, res, next) => {
     try {
         const payload = await jwt.verify(token, process.env.JWT_SECRET);
         // attach the user to the job routes
+
+        // const user = await User.findById(payload.userId).select('-password');
+        // req.user = user;
+
         req.user = {userId: payload.userId, name: payload.name};
         next();
     } catch (error) {
