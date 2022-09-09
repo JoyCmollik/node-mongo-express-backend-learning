678045faaebdafbf6e41ae4fdd20859fa3e70b4d
Joy Chandra Mollik <xoycmollik913@gmail.com>
2022-09-09 22:35:46 +0600

    updated e-commerce

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -1,8 +1,43 @@
+require('dotenv').config();
+require('express-async-errors'); 
+
 // express
 const express = require('express');
 const app = express();
-require('dotenv').config();
+
+// rest of the packages
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+
+// database
 const connectDB = require('./db/connect');
+
+// routers
+const authRouter = require('./routes/authRoutes');
+
+// middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+
+app.use(morgan('tiny'));
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET)); 
+
+app.get('/', (req, res) => {
+    res.send('ecommerce-api');
+})
+
+app.get('/api/v1', (req, res) => {
+    // console.log(req.cookies);
+    console.log(req.signedCookies);
+    res.send('ecommerce-api'); 
+})
+
+app.use('/api/v1/auth', authRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5001;
 
