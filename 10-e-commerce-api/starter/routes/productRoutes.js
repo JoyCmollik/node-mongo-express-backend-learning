2247a78f17ccd9bab393dbeaf7d066f31bfd500e
Joy Chandra Mollik <xoycmollik913@gmail.com>
2022-09-22 21:42:41 +0600

    primary setup for product model, controller, routes

---
+++ 10-e-commerce-api/starter/routes/productRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const { authorizePermissions } = require('../middleware/authentication')
+
+const {
+    createProduct, 
+    getAllProducts,
+    getSingleProduct,
+    updateProduct,
+    deleteProduct,
+    uploadImage
+} = require('../controllers/productController');
+
+router.route('/').get(getAllProducts).post(authorizePermissions('admin') ,createProduct);
+router.route('/:id').get(getSingleProduct).patch(authorizePermissions('admin'), updateProduct).delete(authorizePermissions('admin'), deleteProduct)
+router.route('/uploadImage').post(authorizePermissions('admin'), uploadImage);
+
+module.exports = router;
